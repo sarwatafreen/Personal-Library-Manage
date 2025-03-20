ab2b2758e90a0be70f45cfba71b7549f352eb7bd
sliber50 <160401550+sarwatafreen@users.noreply.github.com>
2025-03-20 16:02:11 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -66,8 +66,8 @@ def add_book(title, author, publication_year, genre, read_status):
     save_library()
     st.session_state.book_added = True
     time.sleep(0.6)
-    # st.experimental_rerun()
-     st.rerun()
+    st.experimental_rerun()
+ 
 def remove_book(index):
     if 0 <= index < len(st.session_state.library):
         del st.session_state.library[index]
